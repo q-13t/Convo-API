21daedbb3329e7d52b39435206e551e7d96b696d
q-13t <vova29092002@gmail.com>
2023-07-27 12:34:38 +0300

    IMP: weightConverter

--- src/main/java/edu/convo/ConversionController.java
+++ src/main/java/edu/convo/ConversionController.java
@@ -2,12 +2,19 @@ package edu.convo;
 
 import java.util.HashMap;
 
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.google.gson.JsonObject;
 
 @RestController
 public class ConversionController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private static final HashMap<String, Double> lengthConversionRate = new HashMap<String, Double>() {
         {
             put("millimeter", 0.0011d);
@@ -19,6 +26,17 @@ public class ConversionController {
             put("feet", 0.3048d);
             put("yard", 0.9144d);
             put("mile", 1609.344d);
+        }
+    };
+
+    private static final HashMap<String, Double> weightConversionRate = new HashMap<String, Double>() {
+        {
+            put("milligram", 0.000001d);
+            put("gram", 0.001d);
+            put("kilogram", 1d);
+            put("ton", 1000d);
+            put("ounce", 0.02835d);
+            put("pound", 0.453592d);
         }
     };
 
@@ -46,12 +64,25 @@ public class ConversionController {
     }
 
     /**
-     * Converts 8 units to one another using {@code meter}s as base conversion rate.
+     * Converts 9 units to one another using {@code meter}s as base conversion rate.
+     * </p>
+     * Available units are:
+     * <ul>
+     * <li>Millimeter</li>
+     * <li>Centimeter</li>
+     * <li>Decimeter</li>
+     * <li>Meter</li>
+     * <li>Kilometer</li>
+     * <li>Inch</li>
+     * <li>Feet</li>
+     * <li>Yard</li>
+     * <li>Mile</li>
+     * </ul>
      * 
-     * @param from  String containing from which unit conversion is
-     * @param to    String containing to which unit conversion is
-     * @param value to convert
-     * @return
+     * @param from  String containing from which unit conversion is.
+     * @param to    String containing to which unit conversion is.
+     * @param value to convert.
+     * @return JSON object containing result of calculation.
      * @see #lengthConversionRate
      */
     @RequestMapping(value = "/length", method = RequestMethod.GET)
@@ -65,9 +96,55 @@ public class ConversionController {
                 from = "meter";
             if (to.isEmpty())
                 to = "meter";
-            double rate = lengthConversionRate.get(from) / lengthConversionRate.get(to);
-            response.addProperty("result", value * rate);
+
+            double convertedValue = value * (lengthConversionRate.get(from) / lengthConversionRate.get(to));
+            response.addProperty("result", convertedValue);
+            logger.info("SUCCESS: converted length : [ " + from + " , " + to + " , " + value + " -> " + convertedValue
+                    + "]");
         } catch (Exception e) {
+            logger.error("ERROR in length conversion: [ " + from + " , " + to + " , " + value + " ]", e);
+            response.addProperty("result", "ERROR");
+        }
+        return response;
+    }
+
+    /**
+     * Converts 6 units to one another using {@code kilogram}s as base conversion
+     * rate.
+     * </p>
+     * Available units are:
+     * <ul>
+     * <li>Milligram</li>
+     * <li>Gram</li>
+     * <li>Kilogram</li>
+     * <li>Ton</li>
+     * <li>Ounce</li>
+     * <li>Pound</li>
+     * </ul>
+     * 
+     * @param from  String containing from which unit conversion is.
+     * @param to    String containing to which unit conversion is.
+     * @param value to convert.
+     * @return JSON object containing result of calculation.
+     * @see #weightConversionRate
+     */
+    @RequestMapping(value = "/weight", method = RequestMethod.GET)
+    JsonObject weightConverter(
+            @RequestParam(value = "from") String from,
+            @RequestParam(value = "to") String to,
+            @RequestParam(value = "value") double value) {
+        JsonObject response = new JsonObject();
+        try {
+            if (from.isEmpty())
+                from = "kilogram";
+            if (to.isEmpty())
+                to = "kilogram";
+            double convertedValue = value * (weightConversionRate.get(from) / weightConversionRate.get(to));
+            response.addProperty("result", convertedValue);
+            logger.info("SUCCESS: converted weight : [ " + from + " , " + to + " , " + value + " -> " + convertedValue
+                    + "]");
+        } catch (Exception e) {
+            logger.error("ERROR in weight conversion: [ " + from + " , " + to + " , " + value + " ]", e);
             response.addProperty("result", "ERROR");
         }
         return response;
